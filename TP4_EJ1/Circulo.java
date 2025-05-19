9f71c2d8d16bc499687a9b564a6dac5e917432db
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-19 16:45:28 -0300

    Añadir clase Círculo con cálculo de área

---
+++ TP4_EJ1/Circulo.java
@@ -0,0 +1,16 @@
+
+public class Circulo extends Figura{
+    private double radio;
+
+
+    public Circulo(){}
+
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+}
