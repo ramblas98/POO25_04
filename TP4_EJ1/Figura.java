3b679bd5f15108e30de552c8d6000e9766cc3979
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-19 16:45:35 -0300

    Añadir clase Figura con métodos para calcular área y perímetro

---
+++ TP4_EJ1/Figura.java
@@ -0,0 +1,19 @@
+public class Figura {
+
+
+    public double area(){
+        return 0;
+    }
+
+    public boolean mismaArea(Figura f){
+        if(this.area() == f.area()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public double perimetro(){
+        return 0;
+    }
+}
