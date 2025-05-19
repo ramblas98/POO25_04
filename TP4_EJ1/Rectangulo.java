e591c071cb22746c117487d61ae898786d02af07
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-19 16:45:42 -0300

    Añadir clase Rectangulo con métodos para calcular área y perímetro

---
+++ TP4_EJ1/Rectangulo.java
@@ -0,0 +1,39 @@
+public class Rectangulo extends Figura{
+
+    private double base;
+    private double altura;
+
+    @Override
+    public double area(){
+        return this.base  * this.altura;
+    }
+
+    @Override
+    public double perimetro(){
+        return (this.base * 2) + (this.altura * 2);
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Rectangulo(){}
+    
+}
