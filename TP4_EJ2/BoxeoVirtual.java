9c201e2954251d4662371f4758122a8c3896486d
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-21 17:18:08 -0300

    Descomentar y corregir la implementación del método realizar en la clase BoxeoVirtual para mostrar frases aleatorias al golpear el saco de boxeo

--- TP4_EJ2/BoxeoVirtual.java
+++ TP4_EJ2/BoxeoVirtual.java
@@ -1,18 +1,17 @@
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class BoxeoVirtual extends Entrenamiento{
     @Override
     public void realizar(){
-        //ArrayList<String> frases = new ArrayList<>();
-        //frases.add("¡Eres lento!");
-        //frases.add("¿Eso es todo lo que tienes?");
-        //frases.add("Mas fuerte!");
+        ArrayList<String> frases = new ArrayList<>();
+        frases.add("¡Eres lento!");
+        frases.add("¿Eso es todo lo que tienes?");
+        frases.add("Mas fuerte!");
         System.out.println(super.nombreAvatar + " acaba de golpear el saco de boxeo");
-        //int min = 1;
-        //int max = frases.size();
-        //int aleatorio = min + (int)(Math.random()*((max-min) + 1));
-        //System.out.println(frases.get(aleatorio));
-        System.out.println("¡Eres lento!");
+        int min = 0;
+        int max = frases.size()-1;
+        int aleatorio = min + (int)(Math.random()*((max-min) + 1));
+        System.out.println(frases.get(aleatorio));
     }
 
     @Override
