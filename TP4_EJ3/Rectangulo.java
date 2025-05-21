85d442413b35428c777de218a28ce9e84c35d170
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-21 16:55:38 -0300

    Añadir clase Rectangulo como extensión de Cuadrilatero con implementación de métodos para área y perímetro

---
+++ TP4_EJ3/Rectangulo.java
@@ -0,0 +1,48 @@
+public class Rectangulo extends Cuadrilatero{
+
+    private double base;
+    private double altura;
+
+    @Override
+    public double area(){
+        return this.base  * this.altura;
+    }
+
+    
+    @Override
+    public double perimetro(){
+        return (this.base * 2) + (this.altura * 2);
+    }
+
+    /*
+     * Metodo area y perimetro tendra error por que no pueden sobreescribir los metodos de la clase cuadrilatero
+     */
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Rectangulo(){}
+
+    @Override
+    public String toString() {
+        return "Rectangulo [base=" + base + ", altura=" + altura + "]";
+    }
+    
+}
