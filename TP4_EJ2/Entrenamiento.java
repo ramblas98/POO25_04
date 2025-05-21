d05f95a1283a1f0a84e6d98182787053f12d5300
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-21 16:32:31 -0300

    Añadir clase Entrenamiento con métodos para gestionar el nombre del avatar

---
+++ TP4_EJ2/Entrenamiento.java
@@ -0,0 +1,14 @@
+public class Entrenamiento {
+    protected String nombreAvatar;
+    public void realizar(){
+
+    }
+
+    public String getNombreAvatar(){
+        return nombreAvatar;
+    }
+
+    public void setNombreAvatar(String n){
+        this.nombreAvatar = n;
+    }
+}
