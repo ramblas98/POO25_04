8687be640a071b850f3a3fdceb746b546bcdc946
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-21 16:32:41 -0300

    Añadir clase TP_EJ2 con implementación de entrenamientos y ejecución de acciones

---
+++ TP4_EJ2/TP_EJ2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class TP_EJ2 {
+    public static void main(String[] args) {
+        Entrenamiento carlosPower = new BoxeoVirtual();
+        carlosPower.setNombreAvatar("Carlos Power");
+        Entrenamiento luciaDance = new ZumbaGalactica();
+        luciaDance.setNombreAvatar("Lucia Dance");
+        Entrenamiento ramiroSnack = new LevantamientoDeComida();
+        ramiroSnack.setNombreAvatar("Ramiro Snack");
+        ArrayList<Entrenamiento> gimnasio = new ArrayList<>();
+        gimnasio.add(carlosPower);
+        gimnasio.add(luciaDance);
+        gimnasio.add(ramiroSnack);
+
+        for(int i = 0; i<gimnasio.size();i++){
+            gimnasio.get(i).realizar();
+        }
+    }
+}
