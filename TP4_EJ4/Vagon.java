c02736ba14bcd69a1c8e12848e514bcd7a882376
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-26 14:11:19 -0300

    Añadir clase Vagon con métodos para gestionar peso máximo y capacidad de pasajeros

---
+++ TP4_EJ4/Vagon.java
@@ -0,0 +1,15 @@
+package TP4_EJ4;
+public class Vagon {
+    public double pesoMaximo() {
+        return 0;
+    }
+
+    public int capacidadPasajeros() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Vagón genérico";
+    }
+}
