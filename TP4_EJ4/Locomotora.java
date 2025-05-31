27bbe75d85a62d0735194c218fb995251b7e17b8
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-05-31 05:39:26 -0300

    Reemplazar la propiedad potencia por peso en la clase Locomotora, añadiendo métodos para gestionar peso, peso máximo de arrastre y velocidad máxima.

--- TP4_EJ4/Locomotora.java
+++ TP4_EJ4/Locomotora.java
@@ -1,17 +1,44 @@
 package TP4_EJ4;
 public class Locomotora {
-    private int potencia;
+    private double peso;
+    private double pesoMaximoArrastre;
+    private double velocidadMaxima;
 
-    public Locomotora(int potencia) {
-        this.potencia = potencia;
+    public Locomotora(double peso, double pesoMaximoArrastre, double velocidadMaxima){
+        this.peso = peso;
+        this.pesoMaximoArrastre = pesoMaximoArrastre;
+        this.velocidadMaxima = velocidadMaxima;
     }
 
-    public int getPotencia() {
-        return potencia;
+    public Locomotora(){}
+
+    public double getPeso(){
+        return peso;
+    }
+    public double getPesoMaximoArrastre(){
+        return pesoMaximoArrastre;
+    }
+    public double getVelocidadMaxima(){
+        return velocidadMaxima;
     }
 
-    @Override
-    public String toString() {
-        return "Locomotora [potencia=" + potencia + "]";
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public void setPesoMaximoArrastre(double pesoMaximoArrastre){
+        this.pesoMaximoArrastre = pesoMaximoArrastre;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima){
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public double arrastreUtil(){
+        return pesoMaximoArrastre - peso;
+    }
+
+    public boolean esEficiente(){
+        return arrastreUtil() >= peso*5;
     }
 }
